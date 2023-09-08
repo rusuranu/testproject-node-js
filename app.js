69c2f82cce9fd2fe4a3ca39a0caa1d9b7b6dd801
rusuranu <primat777@gmail.com>
2023-09-09 02:07:17 +0300

    stat file

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const http=require('http');
+const fs=require('fs');
+const htm=fs.readFileSync('index.html','utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
 const hostname='95.214.11.38';
 const port=8080;
 
 const server = http.createServer((req,res)=>{
-	res.statusCode=200;
-	res.setHEADER=('content-type','text/plane');
-	res.end('hello world!');
+	//res.statusCode=200;
+	res.writeHead(200);
+	res.setHeader=('content-type','text/html');
+	res.end(htm);
 	});
 
 server.listen(port,hostname,()=>{
 	console.log("server running at ${hostname}:${port}");
 	console.log("server running at http://%s:%s/",hostname,port); 
+	console.log(htm);
 	});
